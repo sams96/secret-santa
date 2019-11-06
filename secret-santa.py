7ba05a3bf8c7824feea083cdb00e7ae2052efbac
Sam Smith <sams96@mail.com>
2019-11-06 13:32:51 +0800

    Switched round names and emails for random choice
    Seemed like a more elegant way to do it, and would make it easier to add a feature to stop people getting the same people as last year, as well as a way to record who had who somewhere.

--- secret-santa.py
+++ secret-santa.py
@@ -25,7 +25,7 @@ def choose(names):
 
 		choice = random.choice(others)
 		chosen.append(choice)
-		p.append(choice[1])
+		p.append(choice[0])
 
 	return names
 
@@ -34,10 +34,10 @@ def sendone(i, names, wait):
 		s = smtplib.SMTP(smtp, port = 587, timeout = 60)
 		s.login(addr, pasw)
 
-		msg = MIMEText('Your secret santa is ' + names[0][0], 'plain', 'utf-8')
+		msg = MIMEText('Your secret santa is ' + names[0][3], 'plain', 'utf-8')
 		msg['Subject'] = Header(head, 'utf-8')
 		msg['From'] = addr
-		msg['To'] = names[0][3]
+		msg['To'] = names[0][1]
 		s.send_message(msg)
 
 		#print(msg.as_string())
